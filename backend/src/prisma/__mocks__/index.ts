39feea3ba914f358aa32a19810c72465da8f290c
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-01-31 13:57:11 -0300

    feat: add jest config with prisma

---
+++ backend/src/prisma/__mocks__/index.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "child_process";
+import { join } from "path";
+import { v4 as uuid } from "uuid";
+
+const generateDatabaseUrl = (schema: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Database url not provided");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.append("schema", schema);
+
+  return url.toString();
+};
+
+const schemaId = "test-" + uuid();
+const prismaBinary = join(
+  __dirname,
+  "..",
+  "..",
+  "..",
+  "node_modules",
+  ".bin",
+  "prisma"
+);
+
+const url = generateDatabaseUrl(schemaId);
+process.env.DATABASE_URL = url;
+
+export const prisma = new PrismaClient({
+  datasources: { db: { url } },
+});
+
+beforeAll(() => {
+  execSync(`${prismaBinary} db push --skip-generate`, {
+    env: {
+      ...process.env,
+      DATABASE_URL: generateDatabaseUrl(schemaId),
+    },
+  });
+});
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(
+    `DROP SCHEMA IF EXISTS "${schemaId}" CASCADE;`
+  );
+  await prisma.$disconnect();
+});
