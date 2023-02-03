93aee6edf5644b97923c5b4cbccdf78c895e8265
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-02-03 11:50:11 -0300

    refactor: edit mock constants

--- backend/src/__tests__/mocks/users.mock.ts
+++ backend/src/__tests__/mocks/users.mock.ts
@@ -1,18 +1,18 @@
 import { IUserRequest } from "../../interfaces/users";
 
 export const userMock: IUserRequest = {
-  name: "user",
+  name: "userMock",
   password: "test123",
   emails: "user@test.com",
-  phones: "+55 96 984141178",
+  phones: "+55 96 912345678",
 };
 
-export const userWithoutAccessEmail: IUserRequest = {
+export const userWithoutAccessEmailMock: IUserRequest = {
   ...userMock,
-  emails: ["manyEmails", userMock.emails as string],
+  emails: ["manyEmails@test.com", "otherEmail@test.com"],
 };
 
 export const userWithManyEmailsMock: IUserRequest = {
-  ...userWithoutAccessEmail,
-  accessEmailPosition: 0,
+  ...userWithoutAccessEmailMock,
+  accessEmail: "manyEmails@test.com",
 };
