487b01199ad4136dafed003d47ce1c613d1e665e
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-02-04 18:22:20 -0300

    feat: add retrive, update and delete contact test cases

--- backend/src/__tests__/integration/contacts.test.ts
+++ backend/src/__tests__/integration/contacts.test.ts
@@ -4,10 +4,12 @@ import { createUserPrisma } from "../../utils";
 import {
   contactMock,
   createUserLoginMock,
+  updateContactMock,
   userWithManyEmailsMock,
 } from "../mocks";
 
 let authorization = "Bearer";
+let contactId: string;
 
 beforeAll(async () => {
   await createUserPrisma(userWithManyEmailsMock);
@@ -23,6 +25,7 @@ describe("POST /contacts", () => {
       .post("/contacts")
       .send(contactMock)
       .set("Authorization", authorization);
+    contactId = response.body.id;
 
     expect(response.body).toEqual({
       id: expect.any(String),
@@ -95,7 +98,7 @@ describe("GET /contacts", () => {
     expect(response.body).toHaveLength(1);
     expect(response.body).toEqual([
       {
-        id: expect.any(String),
+        id: contactId,
         name: contactMock.name,
         emails: contactMock.emails,
         phones: contactMock.phones,
@@ -130,6 +133,164 @@ describe("GET /contacts", () => {
     test("with invalid/expired token", async () => {
       const response = await supertest(app)
         .get("/contacts")
+        .set("Authorization", "Bearer potato");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(
+          /^(?=.*invalid)(?=.*expired)(?=.*token).$/i
+        ),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+  });
+});
+
+describe("GET /contacts/:id", () => {
+  test("should be able to get one contact", async () => {
+    const response = await supertest(app)
+      .get(`/contacts/${contactId}`)
+      .set("Authorization", authorization);
+
+    expect(response.body).toEqual({
+      id: contactId,
+      name: contactMock.name,
+      emails: contactMock.emails,
+      phones: contactMock.phones,
+    });
+    expect(response.status).toBe(200);
+  });
+
+  describe("should not be able to get contacts/id", () => {
+    test("without authorization", async () => {
+      const response = await supertest(app).get(`/contacts/${contactId}`);
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*authorization).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("without token", async () => {
+      const response = await supertest(app)
+        .get(`/contacts/${contactId}`)
+        .set("Authorization", "Bearer");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*token).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("with invalid/expired token", async () => {
+      const response = await supertest(app)
+        .get(`/contacts/${contactId}`)
+        .set("Authorization", "Bearer potato");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(
+          /^(?=.*invalid)(?=.*expired)(?=.*token).$/i
+        ),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+  });
+});
+
+describe("PATCH /contacts/:id", () => {
+  test("should be able to update contact", async () => {
+    const response = await supertest(app)
+      .patch(`/contacts/${contactId}`)
+      .send(updateContactMock)
+      .set("Authorization", authorization);
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name: updateContactMock.name,
+      emails: updateContactMock.emails,
+      phones: [updateContactMock.phones],
+    });
+    expect(response.status).toBe(200);
+  });
+
+  describe("should not be able to update contact", () => {
+    test("without authorization", async () => {
+      const response = await supertest(app).patch(`/contacts/${contactId}`);
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*authorization).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("without token", async () => {
+      const response = await supertest(app)
+        .patch(`/contacts/${contactId}`)
+        .set("Authorization", "Bearer");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*token).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("with invalid/expired token", async () => {
+      const response = await supertest(app)
+        .patch(`/contacts/${contactId}`)
+        .set("Authorization", "Bearer potato");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(
+          /^(?=.*invalid)(?=.*expired)(?=.*token).$/i
+        ),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+  });
+});
+
+describe("DELETE /contacts/:id", () => {
+  test("should be able to delete contact", async () => {
+    const response = await supertest(app)
+      .delete(`/contacts/${contactId}`)
+      .set("Authorization", authorization);
+
+    expect(response.body).toBe(undefined);
+    expect(response.status).toBe(204);
+  });
+
+  describe("should not be able to delete contacts/id", () => {
+    test("without authorization", async () => {
+      const response = await supertest(app).delete(`/contacts/${contactId}`);
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*authorization).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("without token", async () => {
+      const response = await supertest(app)
+        .delete(`/contacts/${contactId}`)
+        .set("Authorization", "Bearer");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*token).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("with invalid/expired token", async () => {
+      const response = await supertest(app)
+        .delete(`/contacts/${contactId}`)
         .set("Authorization", "Bearer potato");
 
       expect(response.body).toEqual({
--- backend/src/__tests__/mocks/contacts.mock.ts
+++ backend/src/__tests__/mocks/contacts.mock.ts
@@ -1,7 +1,13 @@
-import { IContactRequest } from "../../interfaces/contacts";
+import { IContactRequest, IContactUpdate } from "../../interfaces/contacts";
 
 export const contactMock: IContactRequest = {
   name: "contact",
   emails: ["contact1@test.com", "contact2@test.com"],
   phones: ["+55 67 912345678", "+55 83 912345678"],
+};
+
+export const updateContactMock: IContactUpdate = {
+  name: "contactUpdated",
+  emails: ["updateContact@test.com"],
+  phones: "+55 43 912345678",
 };
