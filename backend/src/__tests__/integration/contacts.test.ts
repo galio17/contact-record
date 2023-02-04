dcba4e01334526669e3279b75edb62a644995eda
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-02-04 17:43:06 -0300

    feat: add create contact test cases

--- backend/src/__tests__/integration/contacts.test.ts
+++ backend/src/__tests__/integration/contacts.test.ts
@@ -17,6 +17,75 @@ beforeAll(async () => {
   authorization += response.body.token;
 });
 
+describe("POST /contacts", () => {
+  test("should be able to create contact", async () => {
+    const response = await supertest(app)
+      .post("/contacts")
+      .send(contactMock)
+      .set("Authorization", authorization);
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name: contactMock.name,
+      emails: contactMock.emails,
+      phones: contactMock.phones,
+    });
+    expect(response.status).toBe(201);
+  });
+
+  describe("should not be able to create contacts", () => {
+    test("without authorization", async () => {
+      const response = await supertest(app).post("/contacts");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*authorization).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("without token", async () => {
+      const response = await supertest(app)
+        .post("/contacts")
+        .set("Authorization", "Bearer");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(/^(?=.*miss)(?=.*token).$/i),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("with invalid/expired token", async () => {
+      const response = await supertest(app)
+        .post("/contacts")
+        .set("Authorization", "Bearer potato");
+
+      expect(response.body).toEqual({
+        message: expect.stringMatching(
+          /^(?=.*invalid)(?=.*expired)(?=.*token).$/i
+        ),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(401);
+    });
+
+    test("without required fields", async () => {
+      const response = await supertest(app).post("/users");
+
+      expect(response.body).toEqual({
+        message: expect.arrayContaining([
+          expect.stringMatching(/^(?=.*required)(?=.*name).$/i),
+          expect.stringMatching(/^(?=.*required)(?=.*emails).$/i),
+          expect.stringMatching(/^(?=.*required)(?=.*phones).$/i),
+        ]),
+        typeError: expect.any(String),
+      });
+      expect(response.status).toBe(400);
+    });
+  });
+});
+
 describe("GET /contacts", () => {
   test("should be able to list own contacts", async () => {
     const response = await supertest(app)
